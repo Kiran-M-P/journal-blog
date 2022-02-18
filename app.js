ad8c99363e35d733514ed5ed51732d0d94dee25a
Kiran M P <itskiranmp@gmail.com>
2022-02-18 21:30:13 +0530

    done challenge 11

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const ejs = require("ejs");
 
+let posts = [];
+
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent =
@@ -15,14 +17,18 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+// home page
 app.get("/", function (req, res) {
   res.render("home", { startingContent: homeStartingContent });
+  console.log(posts);
 });
 
+// about page
 app.get("/about", function (req, res) {
   res.render("about", { about: aboutContent });
 });
 
+// contact page
 app.get("/contact", function (req, res) {
   res.render("contact", { contact: contactContent });
 });
@@ -34,7 +40,8 @@ app.get("/compose", function (req, res) {
 
 app.post("/compose", function (req, res) {
   const post = { title: req.body.postTitle, content: req.body.postBody };
-  console.log(post);
+  posts.push(post);
+  res.redirect("/");
 });
 app.listen(3000, function () {
   console.log("Server started on port 3000");
